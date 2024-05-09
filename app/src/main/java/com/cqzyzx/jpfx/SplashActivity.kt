ffa6a48a5008e94bc18328f7bd2e491f7d5bd40d
xial@plian.net <xial@plian.net>
2024-05-09 22:44:05 +0800

    init

--- app/src/main/java/com/cqzyzx/jpfx/SplashActivity.kt
+++ app/src/main/java/com/cqzyzx/jpfx/SplashActivity.kt
@@ -8,17 +8,23 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import coil.compose.AsyncImage
 import com.cqzyzx.jpfx.config.AdConfig
 import com.cqzyzx.jpfx.config.Config
 import com.cqzyzx.jpfx.ui.components.NavHostApp
@@ -32,7 +38,7 @@ import com.youxiao.ssp.core.SSPSdk
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : ComponentActivity() {
-
+    var count: Int = 0
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase)
     }
@@ -43,8 +49,24 @@ class SplashActivity : ComponentActivity() {
         login()
         SSPSdk.init(this@SplashActivity, AdConfig.MEDIA_ID, true)
 
-        setContent {
+        mySetContent()
 
+        lifecycle.addObserver(AppLifecycleObserver())
+    }
+
+    private fun clearContent(){
+        setContent{
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column {
+                    Icon(painter = painterResource(id = R.drawable.logo), contentDescription = "")
+                }
+            }
+        }
+    }
+    
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun mySetContent() {
+        setContent {
             /**
              * -1： 显示加载页面
              * 0： 显示广告
@@ -60,20 +82,6 @@ class SplashActivity : ComponentActivity() {
                 }
             }else{
                 Surface {
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        SplashAd { code ->
-                            status = code
-                            /**
-                             * -1: 加载失败
-                             * 0： 加载成功
-                             * 1：加载成功之后隐藏
-                             */
-                            if (code == -1 || code == 1) {
-                                // 显示主界面
-                                status = 1
-                            }
-                        }
-                    }
                     if (status == -1) {
                         //显示加载页面(因为开屏广告加载中，所以要遮住开屏广告的界面)
                         Box(modifier = Modifier.fillMaxSize()) {
@@ -81,9 +89,51 @@ class SplashActivity : ComponentActivity() {
                                 Icon(painter = painterResource(id = R.drawable.logo), contentDescription = "")
                             }
                         }
+                    }else{
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center,
+                            ) {
+                                AsyncImage(model = "https://bootxyysc.oss-cn-hangzhou.aliyuncs.com/logo.png", contentDescription = "")
+                            }
+                        }
+                    }
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        SplashAd { code ->
+                            status = code
+                            if (code == -1 || code == 1) {
+                                // 显示主界面
+                                status = 1
+                            }
+                        }
                     }
                 }
             }
+        }
+    }
+
+
+    private inner class AppLifecycleObserver : LifecycleObserver {
+
+        @RequiresApi(Build.VERSION_CODES.Q)
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun onEnterForeground() {
+            // 应用程序进入前台
+            Log.e("AppLifecycleObserver", "onEnterForeground: 应用程序进入前台")
+            if(count==1){
+                mySetContent()
+                count = 0;
+            }
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun onEnterBackground() {
+            // 应用程序进入后台
+            count = 1
+            clearContent()
+            Log.e("AppLifecycleObserver", "onEnterBackground: 应用程序进入后台")
         }
     }
 
