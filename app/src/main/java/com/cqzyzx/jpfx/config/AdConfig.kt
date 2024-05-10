343fa97bfa819618a2ce918b35f19699285d794b
black <1169794338@qq.com>
2024-05-10 09:03:15 +0800

    init

--- app/src/main/java/com/cqzyzx/jpfx/config/AdConfig.kt
+++ app/src/main/java/com/cqzyzx/jpfx/config/AdConfig.kt
@@ -2,19 +2,19 @@ package com.cqzyzx.jpfx.config
 
 object AdConfig {
 
-    /*const val MEDIA_ID = "7039"
+    const val MEDIA_ID = "7126"
 
     // 测试开屏广告ID
-    const val SPLASH_AD_ID = "13901"
+    const val SPLASH_AD_ID = "14358"
 
     // 测试横幅广告ID
-    const val BANNER_AD_ID = "12904"
+    const val BANNER_AD_ID = "12914"
 
     // 测试模板信息流广告ID
-    const val TEMPLATE_AD_ID = "13905"
+    const val TEMPLATE_AD_ID = "14361"
 
     // 测试插屏广告ID
-    const val INTER_AD_ID = "13902"
+    const val INTER_AD_ID = "14359"
 
     // 模板视频信息流广告ID
     const val VIDEO_FEED_AD_ID = "12906"
@@ -23,34 +23,34 @@ object AdConfig {
     const val FULL_SCREEN_VIDEO = "12907"
 
     // 测试激励视频广告ID
-    const val REWARD_VIDEO_AD_ID = "13903"*/
+    const val REWARD_VIDEO_AD_ID = "14360"
 
 
 
 
 
 
-    const val MEDIA_ID = "6862"
+    /* const val MEDIA_ID = "6862"
 
-    // 测试开屏广告ID
-    const val SPLASH_AD_ID = "12902"
+     // 测试开屏广告ID
+     const val SPLASH_AD_ID = "12902"
 
-    // 测试横幅广告ID
-    const val BANNER_AD_ID = "12904"
+     // 测试横幅广告ID
+     const val BANNER_AD_ID = "12904"
 
-    // 测试模板信息流广告ID
-    const val TEMPLATE_AD_ID = "12905"
+     // 测试模板信息流广告ID
+     const val TEMPLATE_AD_ID = "12905"
 
-    // 测试插屏广告ID
-    const val INTER_AD_ID = "12903"
+     // 测试插屏广告ID
+     const val INTER_AD_ID = "12903"
 
-    // 模板视频信息流广告ID
-    const val VIDEO_FEED_AD_ID = "12906"
+     // 模板视频信息流广告ID
+     const val VIDEO_FEED_AD_ID = "12906"
 
-    // 测试全屏视频广告ID
-    const val FULL_SCREEN_VIDEO = "12907"
+     // 测试全屏视频广告ID
+     const val FULL_SCREEN_VIDEO = "12907"
 
-    // 测试激励视频广告ID
-    const val REWARD_VIDEO_AD_ID = "12909"
+     // 测试激励视频广告ID
+     const val REWARD_VIDEO_AD_ID = "12909"*/
 
 }
