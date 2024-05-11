e06f31e8907aac7c106bf496427114da64a4f08d
black <1169794338@qq.com>
2024-05-11 16:15:47 +0800

    init

--- app/src/main/java/com/cqzyzx/jpfx/config/Config.kt
+++ app/src/main/java/com/cqzyzx/jpfx/config/Config.kt
@@ -1,10 +1,10 @@
 package com.cqzyzx.jpfx.config
 
 object Config {
- //  var baseUrl = "https://yysc.yyll.fun"
+ var baseUrl = "https://yysc.yyll.fun"
     //var baseUrl = "http://yysc.igomall.xin"
     //var baseUrl = "http://192.168.31.214:9902"
-    var baseUrl = "http://172.16.13.94:9902"
+    //  var baseUrl = "http://172.16.13.94:9902"
     var messageTypeList = listOf("系统消息", "收到的点赞", "应用状态", "收到的投币", "好友关注")
     var imageUrl = "https://bootx-tuchuang.oss-cn-hangzhou.aliyuncs.com/"
     var initToken = ""
